184ad09ce5b04aecf05ca5c7c22200584b6c8e7d
Mohamed Sawan <142776172+mohamedsawann@users.noreply.github.com>
2026-02-08 19:43:30 +0300

    Add Netlify function entry

---
+++ netlify-src/server.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import serverless from "serverless-http";
+import { createApp } from "../server/app";
+
+let handler: ReturnType<typeof serverless> | null = null;
+
+export default async (req: unknown, context: unknown) => {
+  if (!handler) {
+    const { app } = await createApp();
+    handler = serverless(app);
+  }
+  return handler(req as Parameters<ReturnType<typeof serverless>>[0], context as Parameters<ReturnType<typeof serverless>>[1]);
+};
